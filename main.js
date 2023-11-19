d246c1892f78272d3dd7b6be4407b0332c27b3d8
jenniferEdward <jenniferedward@pursuit.org>
2023-11-19 06:04:58 -0500

    completed html

---
+++ main.js
@@ -0,0 +1,38 @@
+// main.js
+
+function addNewEntry() {
+    // Get form input values
+    const cookieName = document.getElementById('cookieName').value;
+    const flavor = document.getElementById('flavor').value;
+    const image = document.getElementById('image').value;
+    const price = document.getElementById('price').value;
+    const stock = document.getElementById('stock').value;
+
+    // Create a new cookie entry
+    const newCookieEntry = document.createElement('div');
+    newCookieEntry.className = 'cookie';
+
+    newCookieEntry.innerHTML = `
+        <img src="${image}" alt="${cookieName}">
+        <div class="flavors">
+            <h2 class="flavor">${cookieName}</h2>
+            <p class="price">$${price}</p>
+            <p class="status">${stock}</p>
+            <td><button class="remove-button" onclick="removeItem(this)">Remove</button></td>
+        </div>
+    `;
+
+    // Append the new entry to the store
+    const store = document.querySelector('.store');
+    store.appendChild(newCookieEntry);
+
+    // Reset the form
+    document.querySelector('form').reset();
+}
+
+function removeItem(button) {
+    // Remove the parent element of the button (which is the cookie entry)
+    const cookieEntry = button.parentNode.parentNode;
+    cookieEntry.remove();
+}
+
